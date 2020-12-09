1bfb043b192fc5004237b5b0e9c12c21a9525bc2
svickars <sam@thedataface.com>
2020-12-08 16:06:00 -0800

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -3,27 +3,27 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="" />
-  <meta name="news_keywords" content="" />
+  <meta name="description" content="Make a story using nouns, verbs, adjectives, and adverbs (whatever those are)." />
+  <meta name="news_keywords" content="stories, mad libs" />
 
-  <meta property="og:title" content="" />
+  <meta property="og:title" content="Let's Play Mad Libs!" />
   <meta property="og:site_name" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:description" content="description" />
+  <meta property="og:url" content="https://svickars.github.io/mad-libs/" />
+  <meta property="og:description" content="Make a story using nouns, verbs, adjectives, and adverbs (whatever those are)." />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="" />
+  <meta property="og:image" content="https://svickars.github.io/mad-libs/assets/feature.png" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="" />
-  <meta name="twitter:creator" content="" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image:src" content="" />
+  <meta name="twitter:site" content="https://svickars.github.io/mad-libs/" />
+  <meta name="twitter:creator" content="samvickars" />
+  <meta name="twitter:title" content="Let's Play Mad Libs!" />
+  <meta name="twitter:description" content="Make a story using nouns, verbs, adjectives, and adverbs (whatever those are)." />
+  <meta name="twitter:image:src" content="https://svickars.github.io/mad-libs/assets/feature.png" />
 
   <meta name="robots" content="max-image-preview:large" />
 
